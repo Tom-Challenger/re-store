335971bfa9f629479d38f9111d9c16563e9c4de6
Tom-Challenger <it.mail.art@gmail.com>
2019-08-17 15:09:21 +0300

    Реализовали эмитацию асинхронного получения данных

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 
 import BookListItem from '../book-list-item'
+import Spinner from '../spinner'
 
 import { withBookstoreService } from '../hoc'
 import { booksLoaded } from '../../actions'
@@ -13,16 +14,16 @@ import './book-list.css'
 class BookList extends Component {
 
 	componentDidMount() {
-		// 1. receive data
-		const { bookstoreService } = this.props
-		const data = bookstoreService.getBooks()
-
-		// 2. dispatch action to store
-		this.props.booksLoaded(data)
+		const { bookstoreService, booksLoaded } = this.props
+		bookstoreService.getBooks()
+			.then((data) => booksLoaded(data))
 	}
 
 	render() {
-		const { books } = this.props;
+		const { books, loading } = this.props;
+		if (loading) {
+			return <Spinner />
+		}
 		return (
 			<ul className="book-list">
 				{
@@ -35,8 +36,8 @@ class BookList extends Component {
 	}
 }
 
-const mapStateToProps = ({ books }) => {
-	return { books }
+const mapStateToProps = ({ books, loading }) => {
+	return { books, loading }
 }
 
 const mapDispatchToProps = {

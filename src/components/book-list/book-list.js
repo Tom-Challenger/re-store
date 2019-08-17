82e9234f1e3c77de52ac3d38f19e09ff0fb6336e
Tom-Challenger <it.mail.art@gmail.com>
2019-08-17 18:41:24 +0300

    Подключение компонента CartTable к Redux Store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -7,23 +7,27 @@ import Spinner from '../spinner'
 import ErrorIndicator from '../error-indicator'
 
 import { withBookstoreService } from '../hoc'
-import { booksLoaded, booksRequested, booksError } from '../../actions'
+import { fetchBooks } from '../../actions'
 import { compose } from '../../utils'
 
 import './book-list.css'
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+	return (
+		<ul className="book-list">
+			{
+				books.map((book) => {
+					return <li key={book.id}><BookListItem book={book} /></li>
+				})
+			}
+		</ul>
+	)
+}
+
+class BookListContainer extends Component {
 
 	componentDidMount() {
-		const { 
-			bookstoreService, 
-			booksLoaded, 
-			booksRequested, 
-			booksError } = this.props
-		booksRequested()
-		bookstoreService.getBooks()
-			.then((data) => booksLoaded(data))
-			.catch((err) => booksError(err))
+		this.props.fetchBooks()
 	}
 
 	render() {
@@ -35,15 +39,7 @@ class BookList extends Component {
 		if (error) {
 			return <ErrorIndicator />
 		}
-		return (
-			<ul className="book-list">
-				{
-					books.map((book) => {
-						return <li key={book.id}><BookListItem book={book} /></li>
-					})
-				}
-			</ul>
-		)
+		return <BookList books={books} />
 	}
 }
 
@@ -51,14 +47,15 @@ const mapStateToProps = ({ books, loading, error }) => {
 	return { books, loading, error }
 }
 
-const mapDispatchToProps = {
-	booksLoaded,
-	booksRequested,
-	booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+	const { bookstoreService } = ownProps
+	return {
+		fetchBooks: fetchBooks(bookstoreService, dispatch)
+	}
 }
 
 export default compose(
 	withBookstoreService(),
 	connect(mapStateToProps, mapDispatchToProps)
-)(BookList)
+)(BookListContainer)
 

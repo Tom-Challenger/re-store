8a23566a12aabab2cee1024a7c5353c78539c564
Tom-Challenger <it.mail.art@gmail.com>
2019-08-23 08:09:21 +0300

    Updates

---
+++ precache-manifest.40a9e6ca2ceb49d1aebba4060a0f4bcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35561215731de7f9399955dc5197986c",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "618d5cc4ccdcd11dacb2",
+    "url": "/re-store/static/css/main.1a651e4b.chunk.css"
+  },
+  {
+    "revision": "5596afbe9308eeac52ea",
+    "url": "/re-store/static/js/2.08d53237.chunk.js"
+  },
+  {
+    "revision": "618d5cc4ccdcd11dacb2",
+    "url": "/re-store/static/js/main.83efdf48.chunk.js"
+  },
+  {
+    "revision": "5d6fe284d288fb554198",
+    "url": "/re-store/static/js/runtime~main.d45e52b3.js"
+  }
+]);
